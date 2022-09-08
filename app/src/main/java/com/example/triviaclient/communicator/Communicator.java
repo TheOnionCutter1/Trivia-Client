e4973c06a3c39a3fa88cf20f48ef5f17bc381b5a
Omer <omeralon53@gmail.com>
2022-09-08 11:33:37 +0300

    Create the communicator class

---
+++ app/src/main/java/com/example/triviaclient/communicator/Communicator.java
@@ -0,0 +1,8 @@
+package com.example.triviaclient.communicator;
+
+public class Communicator {
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 8820;
+    private static final String HANDSHAKE_MESSAGE = "Hello";
+    private static final int HANDSHAKE_LENGTH = Communicator.HANDSHAKE_MESSAGE.length();
+}
