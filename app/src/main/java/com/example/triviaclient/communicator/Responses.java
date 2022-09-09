7be4dd3aabbd2cc7deb5749d2b115475595771cc
Omer <omeralon53@gmail.com>
2022-09-09 10:23:18 +0300

    Create the responses structs

---
+++ app/src/main/java/com/example/triviaclient/communicator/Responses.java
@@ -0,0 +1,95 @@
+package com.example.triviaclient.communicator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Responses {
+    public static class Data {
+        public static class RoomData {
+            public int id;
+            public String name;
+            public int maxPlayers;
+            public int questionCount;
+            public int timePerQuestion;
+            public boolean isActive;
+        }
+
+        public static class Statistics {
+            public float avgAnswerTime;
+            public int answerCount;
+            public int correctAnswersCount;
+            public int gameCount;
+        }
+
+        public static class PlayerResults {
+            public String username;
+            public int correctAnswerCount;
+            public int wrongAnswerCount;
+            public int avgTimePerAnswer;
+            public int score;
+        }
+    }
+
+    private static class Success {
+        public int status;
+    }
+
+    public static class Error {
+        public String message;
+    }
+
+    public static class Login extends Success {
+    }
+
+    public static class Signup extends Success {
+    }
+
+    public static class Logout extends Success {
+    }
+
+    public static class GetRooms extends Success {
+        public int status;
+        public ArrayList<Data.RoomData> rooms;
+    }
+
+    public static class GetPlayersInRoom {
+        public ArrayList<String> players;
+    }
+
+    public static class GetHighScore extends Success {
+        public HashMap<String, Integer> statistics;
+    }
+
+    public static class GetPersonalStats extends Success {
+        public Data.Statistics statistics;
+    }
+
+    public static class JoinRoom extends Success {
+    }
+
+    public static class StartGame extends Success {
+    }
+
+    public static class CreateRoom extends Success {
+    }
+
+    public static class GetRoomState extends Success {
+        public boolean isActive;
+        public int questionCount;
+        public int timePerQuestion;
+        public ArrayList<String> players;
+    }
+
+    public static class GetQuestion extends Success {
+        public String question;
+        public HashMap<Integer, String> answers;
+    }
+
+    public static class SubmitAnswer extends Success {
+        public int correctAnswerId;
+    }
+
+    public static class GetGameResults extends Success {
+        public ArrayList<Data.PlayerResults> results;
+    }
+}
