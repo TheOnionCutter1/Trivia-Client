b9ba4192ea6749e985ec8dbf38debca63bdf843a
Omer <omeralon53@gmail.com>
2022-09-10 18:31:11 +0300

    Create a simple login screen
    Follow the communicator's singleton structure

--- app/src/main/java/com/example/triviaclient/LoadingScreen.java
+++ app/src/main/java/com/example/triviaclient/LoadingScreen.java
@@ -2,11 +2,14 @@ package com.example.triviaclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.triviaclient.communicator.Communicator;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 
@@ -14,20 +17,23 @@ public class LoadingScreen extends AppCompatActivity {
     private Communicator _communicator;
     private Toast _connectionFailedMessage;
 
-    public Button connectToServer;
+    private Button _connectToServer;
 
     private void _initializeButtons() {
-        this.connectToServer = this.findViewById(R.id.button_connect);
+        this._connectToServer = this.findViewById(R.id.button_connect);
 
-        this.connectToServer.setOnClickListener(v -> {
+        this._connectToServer.setOnClickListener(v -> {
             Button b = (Button) v;
 
             b.setClickable(false);
             b.setText(R.string.connect_button_pressed);
             b.setBackgroundColor(0xff000000);
             new Thread(() -> {
+                Intent loginScreen = new Intent(this, Login.class);
+
                 try {
                     this._communicator.connectToServer();
+                    this.startActivity(loginScreen);
                 } catch (IOException e) {
                     this._connectionFailedMessage.show();
                     b.setText(R.string.connect_button_unpressed);
@@ -45,9 +51,9 @@ public class LoadingScreen extends AppCompatActivity {
 
         this._connectionFailedMessage = Toast.makeText(LoadingScreen.this,
                 "Connection to the server has failed", Toast.LENGTH_LONG);
-        this._communicator = new Communicator();
+        this._communicator = Communicator.getInstance();
 
         this._initializeButtons();
-        this.connectToServer.performClick();
+        this._connectToServer.performClick();
     }
 }
--- app/src/main/java/com/example/triviaclient/Login.java
+++ app/src/main/java/com/example/triviaclient/Login.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.triviaclient.communicator.Communicator;
+import com.google.gson.Gson;
+
 public class Login extends AppCompatActivity {
+    private Communicator _communicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this._communicator = Communicator.getInstance();
     }
 }
