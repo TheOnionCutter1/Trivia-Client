833e30fcc683d4d9e62cd86a7edd0ff489ac018b
Omer <omeralon53@gmail.com>
2022-09-12 16:46:38 +0300

    Split the code to functions

--- app/src/main/java/com/example/triviaclient/LoadingScreen.java
+++ app/src/main/java/com/example/triviaclient/LoadingScreen.java
@@ -15,35 +15,38 @@ import java.io.IOException;
 public class LoadingScreen extends AppCompatActivity {
     private Toast _connectionFailedMessage;
 
-    private Button _connectToServer;
+    private Button _connectButton;
     private EditText _serverIP;
+
+    private void _connectToServer(Button connectButton) {
+        Intent loginScreen = new Intent(this, Login.class);
+
+        try {
+            Communicator.getInstance().connectToServer(this._serverIP.getText().toString());
+            this.startActivity(loginScreen);
+            this.finish();
+        } catch (IOException e) {
+            this._connectionFailedMessage.show();
+            connectButton.setText(R.string.connect_button_unpressed);
+            connectButton.setBackgroundColor(this.getResources().getColor(R.color.purple_500));
+            connectButton.setClickable(true);
+        }
+    }
 
     /**
      * Initialize the activity's components.
      */
     private void _initializeComponents() {
-        this._connectToServer = this.findViewById(R.id.button_connect);
+        this._connectButton = this.findViewById(R.id.button_connect);
         this._serverIP = this.findViewById(R.id.edittext_server_address);
 
-        this._connectToServer.setOnClickListener(v -> {
+        this._connectButton.setOnClickListener(v -> {
             Button b = (Button) v;
 
             b.setClickable(false);
             b.setText(R.string.connect_button_pressed);
             b.setBackgroundColor(0xff000000);
-            new Thread(() -> {
-                Intent loginScreen = new Intent(this, Login.class);
-
-                try {
-                    Communicator.getInstance().connectToServer(this._serverIP.getText().toString());
-                    this.startActivity(loginScreen);
-                } catch (IOException e) {
-                    this._connectionFailedMessage.show();
-                    b.setText(R.string.connect_button_unpressed);
-                    b.setBackgroundColor(this.getResources().getColor(R.color.purple_500));
-                    b.setClickable(true);
-                }
-            }).start();
+            new Thread(() -> this._connectToServer(b)).start();
         });
     }
 
@@ -56,6 +59,6 @@ public class LoadingScreen extends AppCompatActivity {
                 "Connection to the server has failed", Toast.LENGTH_LONG);
 
         this._initializeComponents();
-        this._connectToServer.performClick();
+        this._connectButton.performClick();
     }
 }
