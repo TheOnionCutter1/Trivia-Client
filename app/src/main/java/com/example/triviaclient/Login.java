b25d4c5bc3eb7c54e03615a40fe9d71acb345f62
Omer <omeralon53@gmail.com>
2022-09-12 16:47:09 +0300

    Implement the login button

--- app/src/main/java/com/example/triviaclient/Login.java
+++ app/src/main/java/com/example/triviaclient/Login.java
@@ -2,14 +2,17 @@ package com.example.triviaclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.triviaclient.communicator.Communicator;
 import com.example.triviaclient.communicator.RequestCode;
 import com.example.triviaclient.communicator.Requests;
 import com.example.triviaclient.communicator.ResponseCode;
+import com.example.triviaclient.communicator.Responses;
 import com.example.triviaclient.communicator.Serializer;
 
 import java.io.IOException;
@@ -20,39 +23,57 @@ public class Login extends AppCompatActivity {
 
     private EditText _username;
     private EditText _password;
+    private TextView _errorTextView;
+    private Button _loginButton;
+
+    private void _login(Requests.Login req) {
+        ArrayList<Byte> response;
+        Intent nextScreen;
+
+        try {
+            this._communicator.sendMessage(
+                    Serializer.serializeRequest(RequestCode.LOGIN, req)
+            );
+            response = this._communicator.receiveMessage();
+            if (response.get(0) == ResponseCode.ERROR.value) {
+                this._loginButton.setClickable(true);
+                this._errorTextView.post(() -> this._errorTextView.setText(
+                        Serializer.<Responses.Error>deserializeResponse(
+                                response, Responses.Error.class
+                        ).message)
+                );
+            } else {
+                nextScreen = new Intent(this, HomeScreen.class);
+                nextScreen.putExtra("username", req.username);
+                this.startActivity(nextScreen);
+                this.finish();
+            }
+        } catch (IOException e) {
+            nextScreen = new Intent(this, LoadingScreen.class);
+            this.startActivity(nextScreen);
+            this.finish();
+        }
+    }
 
     /**
      * Initialize the activity's components.
      */
     private void _initializeComponents() {
+        this._loginButton = this.findViewById(R.id.button_login);
+
+        this._errorTextView = this.findViewById(R.id.textview_login_error);
         this._username = this.findViewById(R.id.edittext_username);
         this._password = this.findViewById(R.id.edittext_password);
-        Button login = this.findViewById(R.id.button_login);
 
-        login.setOnClickListener((v) -> {
+        this._loginButton.setOnClickListener((v) -> {
             Button b = (Button) v;
             Requests.Login req = new Requests.Login();
 
             b.setClickable(false);
-            b.setBackgroundColor(0xff000000);
 
             req.username = this._username.getText().toString();
             req.password = this._password.getText().toString();
-            new Thread(() -> {
-                ArrayList<Byte> response;
-
-                try {
-                    this._communicator.sendMessage(
-                            Serializer.serializeRequest(RequestCode.LOGIN, req)
-                    );
-                    response = this._communicator.receiveMessage();
-                    if (response.get(0) == ResponseCode.ERROR.value) {
-                    } else {
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }).start();
+            new Thread(() -> this._login(req)).start();
         });
     }
 
@@ -62,5 +83,6 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         this._communicator = Communicator.getInstance();
+        this._initializeComponents();
     }
 }
