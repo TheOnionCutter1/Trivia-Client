b8339e8dd11efa03a3dde7a77a8d520e4e4198a1
Omer <omeralon53@gmail.com>
2022-09-29 11:00:47 +0300

    Activate view binding

--- app/src/main/java/com/example/triviaclient/SignupActivity.java
+++ app/src/main/java/com/example/triviaclient/SignupActivity.java
@@ -3,12 +3,20 @@ package com.example.triviaclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.triviaclient.databinding.ActivitySignupBinding;
 
 public class SignupActivity extends AppCompatActivity {
+    private ActivitySignupBinding _binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+
+        this._binding = ActivitySignupBinding.inflate(this.getLayoutInflater());
+        this.setContentView(this._binding.getRoot());
     }
 }
