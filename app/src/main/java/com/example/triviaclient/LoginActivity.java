ce33feca939522769d5c12db5cbe41250fde4b53
Omer <omeralon53@gmail.com>
2022-10-20 06:17:46 +0300

    Remove unnecessary imports

--- app/src/main/java/com/example/triviaclient/LoginActivity.java
+++ app/src/main/java/com/example/triviaclient/LoginActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.triviaclient.communicator.Communicator;
 import com.example.triviaclient.communicator.RequestCode;
