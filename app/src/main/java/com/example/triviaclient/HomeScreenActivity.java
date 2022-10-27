2f57f20f1b87a609d2d611b4ea23233cb1417bf4
Omer <omeralon53@gmail.com>
2022-10-27 17:14:54 +0300

    Create the buttons in the home screen

--- app/src/main/java/com/example/triviaclient/HomeScreenActivity.java
+++ app/src/main/java/com/example/triviaclient/HomeScreenActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.triviaclient.databinding.ActivityHomeScreenBinding;
+
 public class HomeScreenActivity extends AppCompatActivity {
     private String _username;
+    private ActivityHomeScreenBinding _binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        this._binding = ActivityHomeScreenBinding.inflate(this.getLayoutInflater());
+        setContentView(this._binding.getRoot());
 
         this._username = this.getIntent().getStringExtra("username");
+
+        this._binding.textviewHomeScreenTitle.append(this._username + "!");
     }
 }
