e7b8d0c2cf348461d80fd5f79020f8c2ad81bc34
Omer <omeralon53@gmail.com>
2022-11-17 10:58:31 +0200

    Start implementing the personal stats screen

--- app/src/main/java/com/example/triviaclient/PersonalStatsActivity.java
+++ app/src/main/java/com/example/triviaclient/PersonalStatsActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.triviaclient.communicator.Communicator;
+import com.example.triviaclient.communicator.RequestCode;
+import com.example.triviaclient.communicator.Serializer;
+import com.example.triviaclient.databinding.ActivityPersonalStatsBinding;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PersonalStatsActivity extends AppCompatActivity {
+    private ActivityPersonalStatsBinding _binding;
+    private Communicator _communicator;
+
+    private void _fetchStats() {
+        ArrayList<Byte> response;
+
+        try {
+            this._communicator.sendMessage(
+                    Serializer.serializeRequest(RequestCode.GET_PLAYER_STATS, null)
+            );
+        } catch (IOException e) {
+            
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_stats);
+        this._communicator = Communicator.getInstance();
+        this._binding = ActivityPersonalStatsBinding.inflate(this.getLayoutInflater());
+        setContentView(this._binding.getRoot());
+
+        this._fetchStats();
     }
 }
